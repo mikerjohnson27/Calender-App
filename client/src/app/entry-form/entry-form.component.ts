f593b65dd69d5c2f7b9480cf3a5013893ef87683
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-31 17:16:06 -0400

    Basic TS for Form.(nonfunctional but visible.)

---
+++ client/src/app/entry-form/entry-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-form',
+  templateUrl: './entry-form.component.html',
+  styleUrls: ['./entry-form.component.css']
+})
+export class EntryFormComponent {
+  title = 'Calender-App';
+}
