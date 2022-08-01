9b8655fae45af76c57987505baad49a25e509a3b
Mike Johnson <mikerjohnson27unh@gmail.com>
2022-07-31 21:34:32 -0400

    Adding reference to Event.js

---
+++ server/models/Tag.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Tag extends Model {
+}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    start: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    end: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    event_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Event',
+        key: 'id',
+      },
+    },
+    color: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Event',
+  }
+);
+
+module.exports = Tag;
